b3c87a7446eaf50e8df7b341d94d9caa8ea72ae7
namku <syak7771@gmail.com>
2019-09-23 22:28:54 +0200

    Add description variable and description in name variable

--- gcp/modules/gke/variables.tf
+++ gcp/modules/gke/variables.tf
@@ -1,8 +1,16 @@
 variable "name" {
+    description = "the name of the cluster"
     type = "string"
 }
 
+variable "description" {
+    description = "the description of the cluster"
+    type = "string"
+    default = ""
+}
+
 variable "location" {
+    description = "the location (region or zone) to the host the cluster in"
     type = "string"
     default = "europe-west4"
 }
