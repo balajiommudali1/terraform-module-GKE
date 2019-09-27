5c5914bf6628f6823a68932337a98dc45ba8f0c9
namku <syak7771@gmail.com>
2019-09-27 18:19:44 +0200

    Add cluster_autoscaling_enabled variable to true

--- gcp/examples/gke/gke.tf
+++ gcp/examples/gke/gke.tf
@@ -5,6 +5,7 @@ module "gke" {
     name = "gke-test"
     network = data.terraform_remote_state.vpc.outputs.vpc_name
     subnetwork = google_compute_subnetwork.subnetwork.name
+    cluster_autoscaling_enabled = "true"
 
     node_count = "1"
 }
