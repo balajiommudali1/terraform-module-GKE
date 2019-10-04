17443ea6c1331ebb331857ce6a714f4ec9d2fcf3
namku <syak7771@gmail.com>
2019-10-04 11:13:57 +0200

    Set zone in provider witout location in gke module

--- gcp/examples/gke/provider.tf
+++ gcp/examples/gke/provider.tf
@@ -2,12 +2,14 @@ provider "google" {
   version = "~> 2.16"
   project = "kubernetes-252917"
   region  = "europe-west4"
+  zone    = "europe-west4-b"
 }
 
 provider "google-beta" {
   version = "~> 2.16"
   project = "kubernetes-252917"
   region  = "europe-west4"
+  zone    = "europe-west4-b"
 }
 
 terraform {
